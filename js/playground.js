576880b65b0686cabb990458ebe747138da7c67f
tangentlab <guidewire@gmail.com>
2026-01-30 00:23:20 -0600

    Update playground.js

---
+++ js/playground.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Your code here
+    console.log('jQuery is ready!');
+
+    // Example: Handle a button click
+    $('#myButton').click(function() {
+        console.log('Button clicked!');
+    });
+});
